9eaeb70d28cd5a6d2941c3e844829fd5144176a3
Júnior <eurj.lima@gmail.com>
2025-08-14 10:23:26 -0300

    hellocontroller

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+
+const controller = require('./src/controllers/hello.controller');
+app.use(express.json());
+
+app.get('/', controller.sayHello);
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
